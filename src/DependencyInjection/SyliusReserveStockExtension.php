9aca90eff3a13095a3ade80de9097b3967dff321
Stefan Doorn <stefan@efectos.nl>
2018-09-02 08:57:14 +0200

    Rename plugin to SyliusReserveStockPlugin

--- src/DependencyInjection/SyliusReserveStockExtension.php
+++ src/DependencyInjection/SyliusReserveStockExtension.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin\DependencyInjection;
+namespace Loevgaard\SyliusReserveStockPlugin\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class AcmeSyliusExampleExtension extends Extension
+final class SyliusReserveStockExtension extends Extension
 {
     /**
      * {@inheritdoc}
